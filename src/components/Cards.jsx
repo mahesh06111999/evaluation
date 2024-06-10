b394ee35f39d551e88b1c836a522c52801bd5e45
mahesh06111999 <maheshsr611@gmail.com>
2024-06-10 18:59:09 +0530

    Completed

---
+++ src/components/Cards.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "./Card";
+import { DragDropContext } from "react-beautiful-dnd";
+import { useDispatch } from "react-redux";
+import { shiftTask } from "../redux/todoSlice";
+import { useSelector } from "react-redux";
+
+const Cards = ({ stateToMap }) => {
+  // const { currState } = useSelector((card) => card.todo);
+  // console.log(currState);
+
+  const dispatch = useDispatch();
+
+  const shiftingTasks = async (res) => {
+    if (!res.destination) return;
+
+    dispatch(shiftTask({ res }));
+  };
+
+  return (
+    <DragDropContext onDragEnd={shiftingTasks}>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        {stateToMap?.map((todoCard) => {
+          return (
+            <>
+              <Card
+                key={todoCard?.id}
+                title={todoCard?.groupName}
+                todoList={todoCard}
+                groupId={todoCard?.id}
+              />
+            </>
+          );
+        })}
+      </div>
+    </DragDropContext>
+  );
+};
+
+export default Cards;
